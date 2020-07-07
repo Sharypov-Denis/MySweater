829d5e51823e01c8cd34cfa27f2c050aff8e45b2
Sharypov-Denis <sharypov.denis@gmail.com>
2020-07-07 08:09:22 +0300

    Lesson 3 - Spring security

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -8,8 +8,9 @@ import javax.persistence.Id;
 @Entity
 public class Message {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
+
     private String text;
     private String tag;
 
@@ -21,20 +22,20 @@ public class Message {
         this.tag = tag;
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
+    public void setText(String text) {
+        this.text = text;
     }
 
     public String getText() {
         return text;
     }
 
-    public void setText(String text) {
-        this.text = text;
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getTag() {
