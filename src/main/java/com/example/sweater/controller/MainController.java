11dc2afa4c0a424dbe1c8108cdf53710120fdef4
Sharypov-Denis <sharypov.denis@gmail.com>
2020-07-09 09:45:43 +0300

    Lesson 5 - add Spring Boot Freemarker instead Mustache

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -6,11 +6,11 @@ import com.example.sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.jws.soap.SOAPBinding;
 import java.util.Map;
 
 @Controller
@@ -24,10 +24,17 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
         Iterable<Message> messages = messageRepo.findAll();
 
-        model.put("messages", messages);
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
 
         return "main";
     }
@@ -36,7 +43,8 @@ public class MainController {
     public String add(
             @AuthenticationPrincipal User user,
             @RequestParam String text,
-            @RequestParam String tag, Map<String, Object> model) {
+            @RequestParam String tag, Map<String, Object> model
+    ) {
         Message message = new Message(text, tag, user);
 
         messageRepo.save(message);
@@ -45,9 +53,10 @@ public class MainController {
 
         model.put("messages", messages);
 
-        return "main";
+        return "redirect:/main";
     }
-
+}
+/*
     @PostMapping("filter")
     public String filter(@RequestParam String filter, Map<String, Object> model) {
         Iterable<Message> messages;
@@ -62,4 +71,5 @@ public class MainController {
 
         return "main";
     }
-}
+
+ */
